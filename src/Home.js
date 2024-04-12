4d4b958fd970c280f55d78943bf0b2a3da53e69f
tarunvaghaniweb <vaghanitarun13@gmail.com>
2024-04-12 12:21:17 +0530

    elements

--- src/Home.js
+++ src/Home.js
@@ -40,33 +40,24 @@ function Home() {
 
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={handleSignup}>Signup</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" onClick={handleLogin}>Login</a>
-                            </li>
-                            {userEmail && (
-                                <li className="nav-item">
-                                    <button className="nav-link btn btn-link" onClick={handleLogout}>Logout</button>
-                                </li>
-                            )}
-                        </ul>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navbar-nav">
+                        <a className="nav-link active" aria-current="page" href="/profile">Home</a>
+                        {userEmail && <span className="nav-link active">{userEmail}</span>}
+                        {userEmail && <button className="btn btn-danger ms-3" onClick={handleLogout}>Logout</button>}
+                        <button className="btn btn-success ms-5" onClick={handleSignup}>Signup</button>
+                        <button className="btn btn-success ms-2" onClick={handleLogin}>login</button>
                     </div>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
+    </div> 
     )
 }
 
