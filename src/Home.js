fa37ecf07a62b971dae171ff78879b13f0d95ba3
tarunvaghaniweb <vaghanitarun13@gmail.com>
2024-04-12 12:10:41 +0530

    inndex file

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import { auth } from './firebase';
 import { useState, useEffect } from 'react';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 function Home() {
     const [userEmail, setUserEmail] = useState(null);
     const navigate = useNavigate();
@@ -14,9 +14,9 @@ function Home() {
             }
         });
 
-        return () => unsubscribe();    
+        return () => unsubscribe();
     }, []);
-    
+
     const handleLogout = async () => {
         try {
             await auth.signOut();
@@ -25,29 +25,31 @@ function Home() {
             console.error('Error signing out:', error.message);
         }
     };
-  
-    const handleSignup = () =>{
+
+    const handleSignup = () => {
         navigate('/Signup');
     }
-    const handleLogin = () =>{
+    const handleLogin = () => {
         navigate('/Login');
     }
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="/Profile">Navbar</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">Navbar</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="/Profile">Home</a>
-                            {userEmail && <span className="nav-link active">{userEmail}</span>}
-                            {userEmail && <button className="btn btn-danger ms-3" onClick={handleLogout}>Logout</button>}
-                            <button className="btn btn-success ms-5" onClick={handleSignup}>Signup</button>
-                            <button className="btn btn-success ms-2" onClick={handleLogin}>login</button>
-                        </div>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
             </nav>
